e4d680630cf7daf345e4ff64886e273f1fec587d
dcminogue <danielcminogue@gmail.com>
2024-07-30 15:48:05 -0400

    Setup ProgrammingLanguage scaffold and seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+ProgrammingLanguage.create([
+  { title: 'Ruby', description: 'A dynamic, open source programming language with a focus on simplicity and productivity.', link: 'https://www.ruby-lang.org/' },
+  { title: 'Python', description: 'A powerful, fast, easy-to-learn language that makes it possible to do everything from web development to data science.', link: 'https://www.python.org/' },
+  { title: 'JavaScript', description: 'A versatile language used for front-end development, back-end development, and everything in between.', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' }
+])
